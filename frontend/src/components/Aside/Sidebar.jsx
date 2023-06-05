76ab37d64c8c67ed5965b98e38622643c387bc43
devashish bakare <devashishbakare@gmail.com>
2023-06-05 22:00:39 +0530

    added sidebar design

--- frontend/src/components/Aside/Sidebar.jsx
+++ frontend/src/components/Aside/Sidebar.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./sidebar.module.css";
+import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
 const Sidebar = () => {
-  return <div className={style.sideBarContainer}>sidebar</div>;
+  const [genderFilterClick, setGenderFilterClick] = useState(false);
+  const [sportFilterClick, setSportFilterClick] = useState(false);
+  const [colorFilterClick, setColorFilterClick] = useState(false);
+  const [priceFilterClick, setPriceFilterClick] = useState(false);
+
+  return (
+    <>
+      <div className={style.asideContainer}>
+        <div className={style.optionWrapper}>
+          <div
+            className={style.filterOptionContainer}
+            id={style.genderContainer}
+          >
+            <div className={style.textAndSymbol}>
+              <span className={style.filterText}>Gender</span>
+              <span className={style.filterSymbol}>
+                <MdArrowDropDown className={style.dropdownSymbol} />
+              </span>
+            </div>
+          </div>
+          <div className={style.filterOptionContainer}>
+            <div className={style.textAndSymbol}>
+              <span className={style.filterText}>Sports</span>
+              <span className={style.filterSymbol}>
+                <MdArrowDropDown className={style.dropdownSymbol} />
+              </span>
+            </div>
+          </div>
+          <div className={style.filterOptionContainer}>
+            <div className={style.textAndSymbol}>
+              <span className={style.filterText}>Color</span>
+              <span className={style.filterSymbol}>
+                <MdArrowDropDown className={style.dropdownSymbol} />
+              </span>
+            </div>
+          </div>
+          <div className={style.filterOptionContainer}>
+            <div className={style.textAndSymbol}>
+              <span className={style.filterText}>Price</span>
+              <span className={style.filterSymbol}>
+                <MdArrowDropDown className={style.dropdownSymbol} />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
