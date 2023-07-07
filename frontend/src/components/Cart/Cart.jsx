168222ba7875eab395681ecbcf12bba2456b5f42
devashish bakare <devashishbakare@gmail.com>
2023-07-07 10:54:17 +0530

    added cartSum persisted

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -9,6 +9,11 @@ import Sign_in_up from "../Sign_in_up/Sign_in_up";
 import axios from "axios";
 
 const Cart = () => {
+  let storeCartSum = parseInt(localStorage.getItem("cartSum"), 10);
+  const [cartSum, setCartSum] = useState(
+    storeCartSum === null ? 0 : storeCartSum
+  );
+
   //fatching login status
   const LoginStatus = localStorage.getItem("isLoggedIn");
   console.log("logIn Status  " + LoginStatus);
@@ -40,7 +45,7 @@ const Cart = () => {
           sum += quantity * price;
         });
         localStorage.setItem("cartSum", sum);
-        setTotalSum(sum);
+        setCartSum(sum);
       } catch (error) {
         console.log("Error in fetching cart product");
         console.log(error);
@@ -55,27 +60,27 @@ const Cart = () => {
     setShowLoginForm(status);
   };
 
-  const handleQuantitySelection = async (
-    quantity,
-    cartId,
-    cartProductIndex
-  ) => {
-    console.log(quantity + " " + cartId + " " + cartProductIndex);
-    setIsLoading(true);
+  const handleQuantitySelection = async (quantity, cartId, index) => {
+    console.log(quantity + " " + cartId + " " + index);
 
-    if (cartProducts[cartProductIndex].cartItem.quantity > quantity) {
-      let reduceCount =
-        cartProducts[cartProductIndex].cartItem.quantity - quantity;
-      let productPrice = cartProducts[cartProductIndex].cartItem.price;
-      let updatedSum = reduceCount * productPrice;
-      setTotalSum(totalSum - updatedSum);
+    let productOldQuantity = cartProducts[index].cartItem.quantity;
+    let productPrice = cartProducts[index].cartItem.price;
+    if (quantity > productOldQuantity) {
+      let diffrence = quantity - productOldQuantity;
+      let amountIncreaseBy = diffrence * productPrice;
+      let storeSum = parseInt(localStorage.getItem("cartSum", 10));
+      let updatedSum = storeSum + amountIncreaseBy;
+      localStorage.setItem("cartSum", updatedSum);
+      setCartSum(updatedSum);
     } else {
-      let reduceCount =
-        quantity - cartProducts[cartProductIndex].cartItem.quantity;
-      let productPrice = cartProducts[cartProductIndex].cartItem.price;
-      let updatedSum = reduceCount * productPrice;
-      setTotalSum(totalSum + updatedSum);
+      let diffrence = productOldQuantity - quantity;
+      let amountIncreaseBy = diffrence * productPrice;
+      let storeSum = parseInt(localStorage.getItem("cartSum", 10));
+      let updatedSum = storeSum - amountIncreaseBy;
+      localStorage.setItem("cartSum", updatedSum);
+      setCartSum(updatedSum);
     }
+
     const userId = localStorage.getItem("userId");
     try {
       const data = {
@@ -89,14 +94,13 @@ const Cart = () => {
       if (response.status === 200) {
         setCartProducts((prevCart) => {
           const updateCart = [...prevCart];
-          updateCart[cartProductIndex] = response.data[0];
+          updateCart[index] = response.data[0];
           return updateCart;
         });
       }
     } catch (error) {
       console.log(error);
     }
-    setIsLoading(false);
   };
 
   return (
@@ -109,122 +113,136 @@ const Cart = () => {
         </>
       ) : (
         <>
-          <div className={style.cartContainer}>
-            <div className={style.navBarContainer}>
-              <Navbar />
-            </div>
-            <div className={style.cartHeadingText}>Cart</div>
-            <div className={style.cartProductContainer}>
-              <div className={style.productItemSummeryWrapper}>
-                <div className={style.productItemWrapper}>
-                  {cartProducts.map((cartProduct, index) => (
-                    <>
-                      <div
-                        className={style.showProductCart}
-                        key={index + "-" + cartProduct.cartItem._id}
-                      >
-                        <div className={style.cartProductImageWrapper}>
-                          <img
-                            src={cartProduct.productDetails.images[0]}
-                            alt="shoe_image"
-                            className={style.cartProductImag}
-                          />
-                        </div>
-                        <div className={style.cartProductDetailsWrapper}>
-                          <div className={style.headingAndPriceWrapper}>
-                            <div className={style.cartProductName}>
-                              {cartProduct.productDetails.name}
+          {isLoading === true ? (
+            <>
+              <Spinners />
+            </>
+          ) : (
+            <>
+              <div className={style.cartContainer}>
+                <div className={style.navBarContainer}>
+                  <Navbar />
+                </div>
+                <div className={style.cartHeadingText}>Cart</div>
+                <div className={style.cartProductContainer}>
+                  <div className={style.productItemSummeryWrapper}>
+                    <div className={style.productItemWrapper}>
+                      {cartProducts.map((cartProduct, index) => (
+                        <>
+                          <div
+                            className={style.showProductCart}
+                            key={index + "-" + cartProduct.cartItem._id}
+                          >
+                            <div className={style.cartProductImageWrapper}>
+                              <img
+                                src={cartProduct.productDetails.images[0]}
+                                alt="shoe_image"
+                                className={style.cartProductImag}
+                              />
                             </div>
-                            <div className={style.cartProductPrice}>
-                              {cartProduct.productDetails.price} Rs
-                            </div>
-                          </div>
-                          <span className={style.productTypeShoesNaming}>
-                            {cartProduct.productDetails.shoes_type} Road{" "}
-                            {cartProduct.productDetails.category} Shoes
-                          </span>
-                          <div className={style.productSizeAndColorWrapper}>
-                            <span className={style.sizeText}>
-                              Size : {cartProduct.cartItem.size} Uk
-                            </span>
-                            <span className={style.colorText}>
-                              Color : &nbsp;
-                              {
-                                cartProduct.productDetails.availableColors[
-                                  cartProduct.cartItem.color
-                                ]
-                              }
-                            </span>
-                          </div>
-
-                          <div className={style.quantityWrapper}>
-                            <div className={style.increaseIcon}>
-                              <span className={style.quantityText}>
-                                Quantity
+                            <div className={style.cartProductDetailsWrapper}>
+                              <div className={style.headingAndPriceWrapper}>
+                                <div className={style.cartProductName}>
+                                  {cartProduct.productDetails.name}
+                                </div>
+                                <div className={style.cartProductPrice}>
+                                  {cartProduct.productDetails.price} Rs
+                                </div>
+                              </div>
+                              <span className={style.productTypeShoesNaming}>
+                                {cartProduct.productDetails.shoes_type} Road{" "}
+                                {cartProduct.productDetails.category} Shoes
                               </span>
-                              <select
-                                onChange={(event) =>
-                                  handleQuantitySelection(
-                                    event.target.value,
-                                    cartProduct.cartItem._id,
-                                    index
-                                  )
-                                }
-                                className={style.quantitySelectionWrapper}
-                                defaultValue={cartProduct.cartItem.quantity}
-                                value={cartProduct.cartItem.quantity}
-                              >
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                              </select>
-                            </div>
+                              <div className={style.productSizeAndColorWrapper}>
+                                <span className={style.sizeText}>
+                                  Size : {cartProduct.cartItem.size} Uk
+                                </span>
+                                <span className={style.colorText}>
+                                  Color : &nbsp;
+                                  {
+                                    cartProduct.productDetails.availableColors[
+                                      cartProduct.cartItem.color
+                                    ]
+                                  }
+                                </span>
+                              </div>
 
-                            <span className={style.deleteIconWrapper}>
-                              <RiDeleteBin6Line className={style.deleteCart} />
-                            </span>
+                              <div className={style.quantityWrapper}>
+                                <div className={style.increaseIcon}>
+                                  <span className={style.quantityText}>
+                                    Quantity
+                                  </span>
+                                  <select
+                                    onChange={(event) =>
+                                      handleQuantitySelection(
+                                        event.target.value,
+                                        cartProduct.cartItem._id,
+                                        index
+                                      )
+                                    }
+                                    className={style.quantitySelectionWrapper}
+                                    defaultValue={cartProduct.cartItem.quantity}
+                                    value={cartProduct.cartItem.quantity}
+                                  >
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                  </select>
+                                </div>
+
+                                <span className={style.deleteIconWrapper}>
+                                  <RiDeleteBin6Line
+                                    className={style.deleteCart}
+                                  />
+                                </span>
+                              </div>
+                            </div>
                           </div>
+                        </>
+                      ))}
+                    </div>
+                    <div className={style.summeryWrapper}>
+                      <div className={style.cartSummaryTextWrapper}>
+                        <div className={style.summeryHeadingText}>Summary</div>
+                        <div className={style.subTotalWrapper}>
+                          <span className={style.leftWrap}>Subtotal </span>
+                          <span className={style.rightWrap}>
+                            Rs : {cartSum}
+                          </span>
+                        </div>
+                        <div className={style.deliveryChargesWrapper}>
+                          <span className={style.leftWrap}>
+                            Estimated Delivery charge
+                          </span>
+                          <span className={style.rightWrap}>Free</span>
+                        </div>
+                        <div className={style.cartTotalAmountWrapper}>
+                          <span className={style.leftWrap}>Total </span>
+                          <span className={style.rightWrap}>
+                            Rs : {cartSum}
+                          </span>
+                        </div>
+                        <div className={style.summeryButton}>
+                          <button className={style.summeryButtonText}>
+                            Checkout
+                          </button>
                         </div>
                       </div>
-                    </>
-                  ))}
-                </div>
-                <div className={style.summeryWrapper}>
-                  <div className={style.cartSummaryTextWrapper}>
-                    <div className={style.summeryHeadingText}>Summary</div>
-                    <div className={style.subTotalWrapper}>
-                      <span className={style.leftWrap}>Subtotal </span>
-                      <span className={style.rightWrap}>Rs : {totalSum}</span>
-                    </div>
-                    <div className={style.deliveryChargesWrapper}>
-                      <span className={style.leftWrap}>
-                        Estimated Delivery charge
-                      </span>
-                      <span className={style.rightWrap}>Free</span>
-                    </div>
-                    <div className={style.cartTotalAmountWrapper}>
-                      <span className={style.leftWrap}>Total </span>
-                      <span className={style.rightWrap}>Rs : {totalSum}</span>
-                    </div>
-                    <div className={style.summeryButton}>
-                      <button className={style.summeryButtonText}>
-                        Checkout
-                      </button>
                     </div>
                   </div>
                 </div>
+                <div className={style.contactSectionContainer}>
+                  <ContactFooter />
+                </div>
               </div>
-            </div>
-            <div className={style.contactSectionContainer}>
-              <ContactFooter />
-            </div>
-          </div>
+            </>
+          )}
         </>
       )}
     </>
